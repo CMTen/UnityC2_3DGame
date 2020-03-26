79c1361a242005f0a47a6af4ffdf276d567602bc
CMTen <57666274+CMTen@users.noreply.github.com>
2020-03-26 21:05:54 +0800

    learn-static

--- 3DGame_2/Assets/Scripts/LearnCSharp.cs
+++ 3DGame_2/Assets/Scripts/LearnCSharp.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public class LearnCSharp : MonoBehaviour
 {
     // C# 內括號一定成對：() [] {} "" ''
+
+    // 類別：成員
+    // 1. 屬性 (欄位、屬性 Prop)
+    // 2. 方法 (功能)
+    // 3. 事件 (Unity) - 特定時間執行的方法
+
+    // 開始事件：播放遊戲時執行一次
+    private void Start()
+    {
+        print("Hello");
+    }
+
+    private void Update()
+    {
+        print("Ha");
+    }
 }
